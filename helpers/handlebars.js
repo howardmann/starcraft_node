f90350fbd4c6d0d4bfe4bc62c4ae6d7d50c039be
howardmann <howardmann27@gmail.com>
2016-11-23 21:20:38 +1100

    Added capitalize helper method

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,7 +1,11 @@
 // Global handlebars helpers
 var helpers = {
-  _truncate: function(text, chars){
-    return text.substr(0, chars) + ' ...';
+  _truncate: function(string, num){
+    return string.substr(0, num) + ' ...';
+  },
+
+  _capitalize: function(string){
+    return string.split(' ').map(el => el[0].toUpperCase() + el.substr(1, el.length)).join(' ');
   }
 }
 
